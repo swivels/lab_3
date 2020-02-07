f410327e2860d3069c50674ffe2723634bbd29f9
swivels <brianmcelvain@gmail.com>
2020-02-07 15:26:35 -0800

    Adding all the files for lab 3

---
+++ lab3_stript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Authors : Brian McElvain
+# Date: Feb 7th, 2020
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem
+
+echo "Enter a file name: "
+read filename
+
